4eb27949a0ff2e424830da3adbe76f3abac98e31
Tanya Stoyanova <stoyanovatanya17@gmail.com>
2022-04-11 21:25:02 +0300

    Delete 4 Conditional Statements - Exercise directory

--- 4 Conditional Statements - Exercise/05. Time + 15 Minutes.py
+++
@@ -1,9 +0,0 @@
-hour = int(input())
-minute = int(input())
-total_time_in_minutes = hour * 60 + minute + 15
-current_hour = total_time_in_minutes // 60
-current_minutes = total_time_in_minutes % 60
-if current_hour > 23:
-    print(f"0:{current_minutes:02d}")
-else:
-    print(f"{current_hour}:{current_minutes:02d}")
